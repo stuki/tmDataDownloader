4830c99635589abb1e5afb3d837abcc2ea4b9c33
Oscar Storbacka <ostorbacka@gmail.com>
2019-03-07 18:48:59 +0200

    Init

--- TrackData/Program.cs
+++ TrackData/Program.cs
@@ -1,12 +1,140 @@
-﻿using System;
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TrackData
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly Uri url = new Uri("https://mytrackman.com/api/dynamicreports/getreport");
+
+        static HttpClient client = new HttpClient();
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taskList = new List<Task>();
+
+            foreach (var id in args)
+            {
+                taskList.Add(GetReport(id));
+            }
+
+            await Task.WhenAll(taskList);
+        }
+
+        private static async Task GetReport(string item)
+        {
+            var report = new TrackmanReport(item);
+
+            var json = JsonConvert.SerializeObject(report);
+            var buffer = Encoding.UTF8.GetBytes(json);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var result = await client.PostAsync(url, byteContent);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var res = JsonConvert.DeserializeObject<TrackmanData>(result.Content.ReadAsStringAsync().Result);
+                await Save(res);
+            }
+        }
+
+        private static async Task Save(TrackmanData tmData)
+        {
+            var json = JsonConvert.SerializeObject(tmData);
+            var path = $".\\Trackman\\{tmData.Time.ToString("yyyyMMdd")}\\";
+            var jsonFile = $"{path}{tmData.Id}-{tmData.StrokeGroups.First().Club}.json";
+            var csvFile = $"{path}{tmData.Id}-{tmData.StrokeGroups.First().Club}-data.csv";
+            var trajFile = $"{path}{tmData.Id}-{tmData.StrokeGroups.First().Club}-traj.csv";
+
+            var di = Directory.CreateDirectory(path + "Videos");
+
+            if (!File.Exists(jsonFile))
+            {
+                File.WriteAllText(jsonFile, json);
+            }
+
+            var strokes = tmData
+                .StrokeGroups
+                .SelectMany(x => x.Strokes
+                    .Select(v => new VideoDTO { Time = v.Time, Videos = v.Videos }));
+
+            foreach (var stroke in strokes)
+            {
+                string videoFile;
+
+                if (stroke.Videos.Count() > 1)
+                {
+                    for (var i = 0; i < stroke.Videos.Count(); i++)
+                    {
+                        videoFile = $"{path}\\Videos\\{stroke.Time.ToString("yyyy-MM-dd HH.mm.ss")}-{i}.mp4";
+
+                        if (!File.Exists(videoFile))
+                        {
+                            var dl = await GetVideo(stroke.Videos[i].Uri);
+                            using (FileStream output = File.OpenWrite(videoFile))
+                            {
+                                dl.CopyTo(output);
+                            }
+                        }
+                    }
+                } else if (stroke.Videos.Count() == 1)
+                {
+                    videoFile = $"{path}\\Videos\\{stroke.Time.ToString("yyyy-MM-dd HH.mm.ss")}.mp4";
+
+                    if (!File.Exists(videoFile))
+                    {
+                        var dl = await GetVideo(stroke.Videos.First().Uri);
+                        using (FileStream output = File.OpenWrite(videoFile))
+                        {
+                            dl.CopyTo(output);
+                        }
+                    }
+                }
+            }
+
+            var measurments = tmData.StrokeGroups.SelectMany(x => x.Strokes.Select(s => s.NormalizedMeasurement));
+
+            if (!File.Exists(csvFile))
+            {
+                using (var writer = new StreamWriter(csvFile))
+                using (var csv = new CsvWriter(writer))
+                {
+                    csv.WriteRecords(measurments);
+                }
+            }
+
+            var traj = tmData.StrokeGroups.SelectMany(x => x.Strokes.SelectMany(s => s.Measurement.BallTrajectory));
+
+            if (!File.Exists(trajFile))
+            {
+                using (var writer = new StreamWriter(trajFile))
+                using (var csv = new CsvWriter(writer))
+                {
+                    csv.WriteRecords(traj);
+                }
+            }
+        }
+
+        private static async Task<Stream> GetVideo(string url)
+        {
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStreamAsync();
+            }
+
+            throw new Exception();
         }
     }
+
+
 }
---
+++ TrackData/TrackmanData.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace TrackData
+{
+    class TrackmanReport
+    {
+        public string ReportId;
+
+        public TrackmanReport(string reportId)
+        {
+            ReportId = reportId;
+        }
+    }
+    public class TrackmanData
+    {
+        public string Kind { get; set; }
+        public Strokegroup[] StrokeGroups { get; set; }
+        public Settings Settings { get; set; }
+        public Environment Environment { get; set; }
+        public Client Client { get; set; }
+        public User User { get; set; }
+        public Group[] Groups { get; set; }
+        public DateTime Time { get; set; }
+        public string Id { get; set; }
+        public DateTime Updated { get; set; }
+        public string __d_schema { get; set; }
+        public string ManifestId { get; set; }
+        public object[] Sponsors { get; set; }
+        public Facility Facility { get; set; }
+    }
+
+    public class Settings
+    {
+        public string SelectedOptimizedType { get; set; }
+        public string SelectedDispersionMode { get; set; }
+        public object[] SelectedVideoAngles { get; set; }
+        public bool SkidAndRollOn { get; set; }
+        public string ReportSelection { get; set; }
+        public string SelectedView { get; set; }
+        public string UnitSystem { get; set; }
+        public bool ShowNormalizedData { get; set; }
+        public string SelectedClubType { get; set; }
+        public string SelectedOptimizerView { get; set; }
+        public string[] SelectedStrokesGroups { get; set; }
+        public object SelectedStroke { get; set; }
+        public bool OptimizerOn { get; set; }
+        public object[] HiddenShots { get; set; }
+        public object SelectedGroupOfStrokes { get; set; }
+        public bool SummaryOn { get; set; }
+        public bool ShowBottomCommercial { get; set; }
+        public bool VideoOn { get; set; }
+        public bool ClubDataOn { get; set; }
+        public string[] Tiles { get; set; }
+        public bool DispersionOn { get; set; }
+        public bool TrajectoryOn { get; set; }
+        public string InstructorMessage { get; set; }
+        public string Client { get; set; }
+    }
+
+    public class Environment
+    {
+        public float Altitude { get; set; }
+        public float Temperature { get; set; }
+    }
+
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Facility
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public object Description { get; set; }
+        public string Logo { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Strokegroup
+    {
+        public string Id { get; set; }
+        public string Date { get; set; }
+        public string Club { get; set; }
+        public string Ball { get; set; }
+        public Player Player { get; set; }
+        public Stroke[] Strokes { get; set; }
+    }
+
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Stroke
+    {
+        public DateTime Time { get; set; }
+        public string Club { get; set; }
+        public string Ball { get; set; }
+        public Measurement Measurement { get; set; }
+        public Normalizedmeasurement NormalizedMeasurement { get; set; }
+        public string Id { get; set; }
+        public Video[] Videos { get; set; }
+    }
+
+    public class Measurement
+    {
+        public float ClubSpeed { get; set; }
+        public float SwingPlane { get; set; }
+        public float LandingAngle { get; set; }
+        public float SpinLoft { get; set; }
+        public float Total { get; set; }
+        public float SwingDirection { get; set; }
+        public float CarrySide { get; set; }
+        public float LowPointDistance { get; set; }
+        public string Kind { get; set; }
+        public float BallSpeed { get; set; }
+        public float MaxHeight { get; set; }
+        public Balltrajectory[] BallTrajectory { get; set; }
+        public float[] TeePosition { get; set; }
+        public float ClubPath { get; set; }
+        public float DynamicLoft { get; set; }
+        public float Carry { get; set; }
+        public float AttackAngle { get; set; }
+        public float LaunchAngle { get; set; }
+        public int SpinRate { get; set; }
+        public string[] ReducedAccuracy { get; set; }
+        public float HangTime { get; set; }
+        public float Curve { get; set; }
+        public string Id { get; set; }
+        public float FaceAngle { get; set; }
+        public float FaceToPath { get; set; }
+        public float SpinAxis { get; set; }
+        public string PlayerDexterity { get; set; }
+        public float TotalSide { get; set; }
+        public float LaunchDirection { get; set; }
+        public float SmashFactor { get; set; }
+        public float LastData { get; set; }
+        public object[] ClubTrajectory { get; set; }
+    }
+
+    public class Balltrajectory
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+    }
+
+    public class Normalizedmeasurement
+    {
+        public float ClubSpeed { get; set; }
+        public float SwingPlane { get; set; }
+        public float LandingAngle { get; set; }
+        public float SpinLoft { get; set; }
+        public float Total { get; set; }
+        public float SwingDirection { get; set; }
+        public float CarrySide { get; set; }
+        public float LowPointDistance { get; set; }
+        public string Kind { get; set; }
+        public float BallSpeed { get; set; }
+        public float MaxHeight { get; set; }
+        public float[] TeePosition { get; set; }
+        public float ClubPath { get; set; }
+        public float DynamicLoft { get; set; }
+        public float Carry { get; set; }
+        public float AttackAngle { get; set; }
+        public float LaunchAngle { get; set; }
+        public float SpinRate { get; set; }
+        public string[] ReducedAccuracy { get; set; }
+        public float HangTime { get; set; }
+        public float Curve { get; set; }
+        public string Id { get; set; }
+        public float FaceAngle { get; set; }
+        public float FaceToPath { get; set; }
+        public float SpinAxis { get; set; }
+        public string PlayerDexterity { get; set; }
+        public float TotalSide { get; set; }
+        public float LaunchDirection { get; set; }
+        public float SmashFactor { get; set; }
+        public float LastData { get; set; }
+    }
+
+    public class Video
+    {
+        public string Id { get; set; }
+        public string Angle { get; set; }
+        public string Uri { get; set; }
+        public string Poster { get; set; }
+        public Metadata Metadata { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string CameraName { get; set; }
+        public float FramesPrSecond { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public float Rotation { get; set; }
+        public string Duration { get; set; }
+        public float SpeedFactor { get; set; }
+    }
+
+    public class Group
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class VideoDTO
+    {
+        public Video[] Videos { get; set; }
+        public DateTime Time { get; set; }
+    }
+
+}
